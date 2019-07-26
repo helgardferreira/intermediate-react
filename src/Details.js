9ca368e5d59d0f265aa716fc517efa0ea1874184
Helgard Ferreira <hrfer3@gmail.com>
2019-07-26 15:59:07 +0200

    Add Error Boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 // NB: Hooks don't work inside of class components
 class Details extends React.Component {
@@ -41,4 +42,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
