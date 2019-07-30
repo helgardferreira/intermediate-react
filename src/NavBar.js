3dc59b7a8676587a7d3e55bf3fe8ea37b8d9d69c
Helgard Ferreira <hrfer3@gmail.com>
2019-07-30 17:37:24 +0200

    Experiment With Emotion CSS

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
+import colors from './colors';
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">
-      🐩
-    </span>
-  </header>
-);
+const spin = keyframes`
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const NavBar = () => {
+  // const [padding, setPadding] = useState(15);
+  const [padding] = useState(15);
+  const [spinInterval, setSpinInterval] = useState(1);
+
+  return (
+    <header
+      // onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: ${colors.secondary};
+        padding: ${padding}px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      {/* eslint-disable */}
+      <span
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: ${spinInterval}s ${spin} linear infinite;
+
+          &:hover {
+            cursor: pointer;
+          }
+        `}
+        role="img"
+        aria-label="logo"
+        onClick={() => {
+          setSpinInterval(spinInterval / 1.25);
+        }}
+      >
+        🐩
+      </span>
+      {/* eslint-enable */}
+    </header>
+  );
+};
 
 export default NavBar;
